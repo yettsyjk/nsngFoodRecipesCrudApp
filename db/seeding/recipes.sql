d55cbe0a865f346f3b69eb0ab02f91284c426a36
yettsyjk <56506858+yettsyjk@users.noreply.github.com>
2019-12-24 15:06:22 -0700

    server is back up but times out

--- db/seeding/recipes.sql
+++ db/seeding/recipes.sql
@@ -11,24 +11,24 @@ INSERT INTO recipes (title, author, description, category_type, ingredients, pho
     (
         'Cheesy Bacon Meatloaf', 
         'Jane Jetson', 
-        'Combine beef, egg, onion, milk and bread crumbs. Season with salt and pepper. Form into a loaf and place into baking dish. Pour brown sugar, mustard, and ketchup mixture over loaf. Bake at 350 degrees for 1 hour.', 
+        'Combine beef, egg, onion, milk and almond flour. Season with salt and pepper. Form into a loaf and place into baking dish. Pour mustard, and tomato mixture over loaf. Bake at 350 degrees for 1 hour.', 
         3,
-        '{"1 1/2 pounds ground beef","1 egg","1 onion",chopped,"1 cup milk","1 cup dried bread crumbs","salt and pepper to taste","2 tablespoons brown sugar","2 tablespoons prepared mustard","1/3 cup ketchup"}',
+        '{"1 1/2 pounds ground beef","1 egg","1 onion",chopped,"1 cup milk","1 cup almond flour","salt and pepper to taste","2 tablespoons brown sugar","2 tablespoons prepared mustard","1/3 cup tomotoes"}',
         'https://media.giphy.com/media/zG1Ahac9UHcIw/giphy.gif'
     ),
     (
         'Chicken Fancesca', 
         'Barney Rubble', 
-        'Evenly coat milk-soaked chicken in mix of bread crumbs, pecorino romano cheese, and parsley. Cook in large skillet until lightly browned. Bake in oven for 25 minutes.', 
+        'Evenly coat milk-soaked chicken in mix of almond flour, pecorino romano cheese, and parsley. Cook in large skillet until lightly browned. Bake in oven for 25 minutes.', 
         4,
-        '{"4 large skinless","boneless chicken breast halves","1 cup milk","or as needed","1 cup bread crumbs","1 cup grated Pecorino Romano cheese","2 tablespoons chopped fresh parsley","1 cup olive oil","salt and ground black pepper to taste","1 lemon","thinly sliced","1 cup white wine","1 cup chicken broth","1/4 cup lemon juice","1 ounce brandy-based orange liqueur (such as Grand Marnier®)","or to taste"}',
+        '{"4 large skinless","boneless chicken breast halves","1 cup milk","or as needed","1 cup almond flour","1 cup grated Pecorino Romano cheese","2 tablespoons chopped fresh parsley","1 cup olive oil","salt and ground black pepper to taste","1 lemon","thinly sliced","1 cup white wine","1 cup chicken broth","1/4 cup lemon juice","1 ounce brandy-based orange liqueur (such as Grand Marnier®)","or to taste"}',
         'http://images.media-allrecipes.com/userphotos/560x315/2179438.jpg'
     ),
     (
         'Pineapple Grilled Pork Chops', 
         'Oscar Proud', 
-        'Mix together the drained pineapple juice, brown sugar, soy sauce, and garlic powder together in a large plastic zipper bag, and smush the bag a few times with your hands to mix the marinade and dissolve the sugar. Place the pork chops into the marinade, squeeze out any air in the bag, seal it, and refrigerate overnight. Reserve the pineapple rings. Preheat an outdoor grill for medium heat, and lightly oil the grate.  Remove the chops from the marinade, shaking off excess, and grill until browned, the meat is no longer pink inside, and the meat shows good grill marks, 5 to 8 minutes per side. Brush several times with marinade and let the marinade cook onto the surface of the meat. Discard excess marinade. While the meat is grilling, place 4 pineapple rings onto the grill, and allow to cook until hot and the slices show grill marks; serve the chops topped with the grilled pineapple rings.', 
+        'Mix together the drained pineapple juice, soy sauce, and garlic powder together in a large plastic zipper bag, and smush the bag a few times with your hands to mix the marinade. Place the pork chops into the marinade, squeeze out any air in the bag, seal it, and refrigerate overnight. Reserve the pineapple rings. Preheat an outdoor grill for medium heat, and lightly oil the grate.  Remove the chops from the marinade, shaking off excess, and grill until browned, the meat is no longer pink inside, and the meat shows good grill marks, 5 to 8 minutes per side. Brush several times with marinade and let the marinade cook onto the surface of the meat. Discard excess marinade. While the meat is grilling, place 4 pineapple rings onto the grill, and allow to cook until hot and the slices show grill marks; serve the chops topped with the grilled pineapple rings.', 
         5,
-        '{"1 (8 ounce) can pineapple rings","juice drained and reserved","1/4 cup brown sugar","1/4 cup soy sauce","1/4 teaspoon garlic powder","4 pork chops","1 pinch ground black pepper"}',
+        '{"1 (8 ounce) can pineapple rings","juice drained and reserved","1/4 cup soy sauce","1/4 teaspoon garlic powder","4 pork chops","1 pinch ground black pepper"}',
         'http://images.media-allrecipes.com/userphotos/250x250/857812.jpg'
     );
--- db/seeding/seed.sql
+++ db/seeding/seed.sql
@@ -1,2 +1,5 @@
+-- connecting to database
+-- \connect recipe_development;
+-- importing categories and recipes sql information
 \i categories.sql;
 \i recipes.sql;
